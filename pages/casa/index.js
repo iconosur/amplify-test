bd33444669ef4b0b44681898bc4feaa09a79a31e
Agustin DElia <agusrdelia@gmail.com>
2021-12-31 17:33:05 -0300

    Update

--- pages/casa/index.js
+++ pages/casa/index.js
@@ -1,3 +1,15 @@
-export default function Casa() {
-	return <>U r in home</>
+export default function Casa({ year, month, day, id }) {
+	return <>
+		<p>year: ${year}</p>
+		<p>month: ${month}</p>
+		<p>day: ${day}</p>
+		<p>id: ${id}</p>
+	</>
+}
+export async function getServerSideProps( { query } ) {
+	const { year, month, day, id } = query;
+
+	return {
+		props: query,
+	}
 }
