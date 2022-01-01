041940c2d8d89bb3af766eecfa370e95dbe29359
Agustin DElia <agusrdelia@gmail.com>
2022-01-01 11:26:11 -0300

    Update

---
+++ pages/casa/[path].js
@@ -0,0 +1,19 @@
+import Amplify from 'aws-amplify';
+import config from '../src/aws-exports';
+Amplify.configure({...config, ssr: true});
+
+export default function Casa({ year, month, day, id }) {
+	return <>
+		<p>year: ${year}</p>
+		<p>month: ${month}</p>
+		<p>day: ${day}</p>
+		<p>id: ${id}</p>
+	</>
+}
+export async function getServerSideProps( { query } ) {
+	const { year, month, day, id } = query; 
+
+	return {
+		props: query,
+	}
+}
