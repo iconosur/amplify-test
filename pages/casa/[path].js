733db3307d95705bdf177a4ee9b7d7e58ed34c1a
Agustin DElia <agusrdelia@gmail.com>
2022-02-11 00:17:56 -0300

    Update 2

--- pages/casa/[path].js
+++ pages/casa/[path].js
@@ -1,17 +1,28 @@
-import { useRouter } from 'next/router'
+import Link from 'next/link';
 import Amplify from 'aws-amplify';
 import config from '../../src/aws-exports';
 Amplify.configure({...config, ssr: true});
 
-export default function Casa(props) {
-	console.log(props);
+export default function Casa({path}) {
+	console.log(path);
+	const random = Date.now()
 	return <>
-		<p>id</p>
+		<p>id: {path}</p>
+		<Link href="/">
+			<a>Home</a>
+		</Link>
+		<br />
+		<Link href={`/casa/${Date.now()}.html`}>
+			<a>Nota random: {Date.now()}</a>
+		</Link>
 	</>
 }
 export async function getServerSideProps( { query } ) {
+	const { path } = query;
 	return {
-		props: query,
+		props: {
+			path
+		},
 	}
 }
 
