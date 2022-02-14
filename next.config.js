d7537141a45c2627978797544c5883f9a319f8fb
Agustin DElia <agusrdelia@gmail.com>
2022-02-14 10:26:29 -0300

    tet

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/category/:path*',
-        destination: '/section/:path*',
-      },
-    ]
-  },
+  reactStrictMode: true
 }
