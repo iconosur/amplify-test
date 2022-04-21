bf744a9be4a4ac5c0002241b14cd3487ec30d708
Agustin DElia <agusrdelia@gmail.com>
2022-04-21 11:37:58 -0300

    Update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/category/:name*",
-          destination: "/section",
-        },
-        {
-          source: "/tag/:name*",
-          destination: "/section/[name].js",
-        },
-      ],
-    };
-  },
 }
 
 module.exports = nextConfig
